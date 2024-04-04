aa133e82499191e79481c5cfee38c46fcc99783c
IvaSv <76878466+IVA78@users.noreply.github.com>
2024-04-04 12:02:25 +0200

    Initial commit
    Inicijalizacija backenda i frontenda: postavljanje react applikacije za frontend i spring-boot aplikacije za backend

---
+++ Zavrsni rad/IzvorniKod/Backend/spring-voznja-plus/src/test/java/zavrsni/rad/ZavrsniApplicationTests.java
@@ -0,0 +1,13 @@
+package zavrsni.rad;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZavrsniApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
