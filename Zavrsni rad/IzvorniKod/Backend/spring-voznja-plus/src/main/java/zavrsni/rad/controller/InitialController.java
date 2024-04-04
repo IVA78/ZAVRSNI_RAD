bf07bcdb3745caeebdea566b8cad6bd3ae6d9f5b
IvaSv <76878466+IVA78@users.noreply.github.com>
2024-04-04 15:27:15 +0200

    Inicijalna poveznica frontenda i backenda
    Ispis poruke sa backenda na frontendu za provjeru proxy-ja koji preuspjerava zahtjeve sa frontenda na backend

---
+++ Zavrsni rad/IzvorniKod/Backend/spring-voznja-plus/src/main/java/zavrsni/rad/controller/InitialController.java
@@ -0,0 +1,18 @@
+package zavrsni.rad.controller;
+
+import org.apache.coyote.Response;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("initial")
+public class InitialController {
+
+    @GetMapping()
+    public ResponseEntity<String> getInitialValue() {
+        return ResponseEntity.ok("Hello from the backend");
+    }
+
+}
--- Zavrsni rad/IzvorniKod/Frontend/react-voznja-plus/src/App.js
+++ Zavrsni rad/IzvorniKod/Frontend/react-voznja-plus/src/App.js
@@ -1,10 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
+
+import Message from './components/Message';
 
 function App() {
   return (
-    <div className="App">
-      <h1>This is frontend!</h1>
+    <div>
+      <Message></Message>
     </div>
   );
 }
