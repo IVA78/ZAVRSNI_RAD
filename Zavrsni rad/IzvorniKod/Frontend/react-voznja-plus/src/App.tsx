3bcbc604e4afc40dcaa04eb4229b0b1d63e817d5
IvaSv <76878466+IVA78@users.noreply.github.com>
2024-05-06 10:47:37 +0200

    Promjena na frontendu: nova incijalizacija projekta (TypeScript)

---
+++ Zavrsni rad/IzvorniKod/Frontend/react-voznja-plus/src/App.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Message from "./components/Message";
+
+function App() {
+  return <Message></Message>;
+}
+
+export default App;
---
+++ Zavrsni rad/IzvorniKod/Frontend/react-voznja-plus/src/components/Message.tsx
@@ -0,0 +1,37 @@
+import React, { useState, useEffect } from "react";
+
+function Message() {
+  const [initialValue, setInitialValue] = useState("");
+
+  useEffect(() => {
+    console.log("Component mounted");
+
+    fetch("/api/initial", {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/text",
+      },
+    })
+      .then((response) => {
+        if (response.ok) {
+          console.log("Response ok!");
+          console.log("Response: ", response.text());
+          return response.text(); // Expecting a text response
+        } else {
+          console.error("Server error:", response.status, response.statusText);
+          throw new Error("Server error");
+        }
+      })
+      .then((data) => {
+        setInitialValue(data); // Set the received text as initialValue
+        console.log("Data: ", data);
+      })
+      .catch((error) => {
+        console.error("Error: " + error);
+      });
+  }, []);
+
+  return <h1>{initialValue}Hello World!</h1>;
+}
+
+export default Message;
