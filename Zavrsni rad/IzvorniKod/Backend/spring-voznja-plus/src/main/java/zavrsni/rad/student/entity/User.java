39e04218d290d0bb3b95ca28ae64e7d31014d68b
IvaSv <76878466+IVA78@users.noreply.github.com>
2024-05-07 16:44:39 +0200

    Definiranje entiteta - korisnik
    Ovisno o dodijeljenoj ulozi, korisnik je student, instruktor ili admin

---
+++ Zavrsni rad/IzvorniKod/Backend/spring-voznja-plus/src/main/java/zavrsni/rad/student/entity/User.java
@@ -0,0 +1,36 @@
+package zavrsni.rad.student.entity;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+
+
+//user can be student, instructor or administrator - DEFINED BY THE ROLE
+@Entity
+@Table(name="user")
+public class User {
+    @Id
+    @Column(name="id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name="role", nullable = false)
+    private String role;
+
+    @Column(name="firstname", nullable = false)
+    private String firstName;
+    @Column(name="lastname", nullable = false)
+    private String lastName;
+
+    @Column(name="dateofbirth", nullable = false)
+    private LocalDate dateOfBirth;
+
+    @Column(name="email", nullable = false, unique = true)
+    private String email;
+
+    @Column(name="phonenumber", nullable = false)
+    private String phoneNumber;
+
+    @Column(name = "note", columnDefinition = "TEXT")
+    private String note;
+}
