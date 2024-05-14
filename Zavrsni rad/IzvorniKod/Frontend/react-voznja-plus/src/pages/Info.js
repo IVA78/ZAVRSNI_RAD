2cdff9cc008058e8842cfab2b68df42043caf0df
IvaSv <76878466+IVA78@users.noreply.github.com>
2024-05-14 17:06:25 +0200

    Doradjivanje Info stranice

--- Zavrsni rad/IzvorniKod/Frontend/react-voznja-plus/src/pages/Info.js
+++ Zavrsni rad/IzvorniKod/Frontend/react-voznja-plus/src/pages/Info.js
@@ -19,7 +19,7 @@ export default function Info() {
       >
         <GridItem
           colSpan={{ base: '5', lg: '5' }}
-          minHeight={{ base: '50vh', lg: '100vh' }}
+          maxH={{ base: '100vh', lg: '100vh' }}
           position="relative"
         >
           <Box
@@ -27,8 +27,44 @@ export default function Info() {
             width="100%"
             height="100%"
             borderRadius="1.5em"
-            loadingText="Šalje se..."
-          ></Box>
+            display="flex"
+            justifyContent="center"
+            alignItems="center"
+          >
+            <Text fontSize="xl" padding="4em">
+              Autoškola "Vožnja +" nije samo obrazovna institucija: to je simbol
+              pouzdanosti, kvalitete i posvećenosti sigurnosti u prometu već
+              više od dva desetljeća. Naša povijest obiluje uspjesima, izazovima
+              i kontinuiranim prilagodbama kako bismo osigurali da naši
+              polaznici dobiju najbolju moguću obuku.
+              <br />
+              <br />
+              Kroz godine, naša autoskola postala je sinonim za vrhunsku obuku
+              vozačkih vještina i prometne sigurnosti. Osnovana s jasnim ciljem
+              - pružiti vrhunsku obuku i stvoriti sigurne i odgovorne vozače -
+              postali smo priznata institucija koja je iznjedrila mnoge vozače
+              koji voze s povjerenjem i sigurnošću.
+              <br />
+              <br />
+              Neprestano se prilagođavamo promjenjivim potrebama i zahtjevima
+              suvremenog prometa. Naša obuka se kontinuirano usavršava kako
+              bismo osigurali da naši polaznici dobiju najnovije informacije,
+              tehnike i vještine potrebne za sigurno upravljanje vozilima u
+              različitim prometnim situacijama.
+              <br />
+              <br />
+              Ponosimo se našom prošlošću i tradicijom, ali istovremeno gledamo
+              prema budućnosti s entuzijazmom i predanošću. Uvijek težimo biti
+              korak ispred, uvodeći nove metode obuke, tehnologije i pristupe
+              kako bismo osigurali da naši vozači budu najbolje pripremljeni za
+              izazove na cestama.
+              <br />
+              <br />
+              Autoskola "Sigurna Vožnja" ostaje predana svojoj misiji -
+              osposobiti sigurne, odgovorne i samostalne vozače koji će
+              doprinijeti sigurnosti i kvaliteti života na cestama.
+            </Text>
+          </Box>
         </GridItem>
 
         <GridItem
@@ -50,15 +86,18 @@ export default function Info() {
           flexDirection="column"
           justifyContent="center"
           alignItems="center"
+          paddingRight="2em"
         >
           <Text fontSize="xl">
             Imate li dodatna pitanja ili želite saznati više o našim uslugama?
-            Ne ustručavajte se kontaktirati nas! Naš tim stručnjaka spreman je
-            odgovoriti na vaša pitanja i pružiti vam sve potrebne informacije.
-            Kliknite na gumb "Pošalji upit" kako biste nam poslali upit.
+            Naš tim stručnjaka spreman je odgovoriti na vaša pitanja i pružiti
+            vam sve potrebne informacije.
             <br />
             <br />
-            Veselimo se što ćemo vam pomoći u ostvarivanju vaših ciljeva.
+            Kliknite na gumb "Pošalji upit".
+            <br />
+            <br />
+            Veselimo se što ćemo Vam pomoći u ostvarivanju vaših ciljeva.
           </Text>
           <NavLink to="/contact">
             <Button
